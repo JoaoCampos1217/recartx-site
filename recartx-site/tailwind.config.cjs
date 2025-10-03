88bcdf260e18acb505259295fe688aa5a0e9f59f
JoaoCampos1217 <joaocorintins@gmail.com>
2025-10-03 16:32:27 +0000

    primeiro deploy recartx

---
+++ recartx-site/tailwind.config.cjs
@@ -0,0 +1,16 @@
+module.exports = {
+  content: ["./index.html", "./src/**/*.{js,jsx}"],
+  theme: {
+    extend: {
+      colors: {
+        recartxPurple: {
+          DEFAULT: '#6B21A8'
+        },
+        recartxGreen: {
+          DEFAULT: '#10B981'
+        }
+      }
+    }
+  },
+  plugins: []
+};
